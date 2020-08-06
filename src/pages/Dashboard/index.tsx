14321a5bdaaf03557d5c7b0e616baeadf29b5ee5
Bárbara N. Sousa <barbara.ns@outlook.com>
2020-08-05 23:54:39 -0300

    Adicionando estilização da dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <div />;
-}
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form action="">
+        <input type="text" placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/53186630?s=460&u=f287a665c7076722c2eeebd219e45de8c8cc4ca7&v=4"
+            alt=""
+          />
+          <div>
+            <strong>Babi</strong>
+            <p>Gobarber</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
+};
 
 export default Dashboard;
